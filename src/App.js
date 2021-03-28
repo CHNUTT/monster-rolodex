f20660ce0673ea02c93fbaf7c3bb9af81301f459
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-03-28 18:05:18 -0500

    [ Styles ] - adjusting styles/ fonts/ and more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardList from './components/card-list/card-list.compoenet';
+import SearchBox from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -9,7 +10,10 @@ class App extends React.Component {
 
     this.state = {
       monsters: [],
+      searchFiled: '',
     };
+
+    // this.handleOnChange = this.handleOnChange.bind(this);
   }
 
   componentDidMount() {
@@ -20,10 +24,23 @@ class App extends React.Component {
       });
   }
 
+  handleOnChange = (e) => {
+    this.setState({ searchFiled: e.target.value });
+  };
+
   render() {
+    const { monsters, searchFiled } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    );
     return (
-      <div className='app'>
-        <CardList monsters={this.state.monsters}/>
+      <div className='App'>
+        <h1> Monster Rolodex </h1>
+        <SearchBox
+          placeholder='search monster'
+          handleOnChange={this.handleOnChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
