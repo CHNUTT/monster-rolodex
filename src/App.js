66426553692cbb7bf42f2f211fd281f2aec66c42
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-03-28 15:20:54 -0500

    [ CardList ] - implement card-list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import CardList from './components/card-list/card-list.compoenet';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((data) => data.json())
+      .then((monsters) => {
+        this.setState({ monsters: monsters });
+      });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <CardList monsters={this.state.monsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
